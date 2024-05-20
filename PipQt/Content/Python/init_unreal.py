caefb660daa2ed85d1d9140f1beec86e957204fc
hannes <hannesdelbeke@gmail.com>
2024-05-20 10:57:37 +0100

    init

--- PipQt/Content/Python/init_unreal.py
+++ PipQt/Content/Python/init_unreal.py
@@ -1,4 +1,3 @@
-# code in init_unreal.py wil run on startup if the plugin is enabled
 import unreal
 
 
@@ -6,9 +5,9 @@ def create_script_editor_button():
     """Add a tool button to the tool bar"""
 
     section_name = 'Plugins'
-    se_command = 'import my_module;my_module.show()'  # todo replace with your code
-    label = 'My Plugin'
-    tooltip = "my tooltip"
+    se_command = 'import pip_qt;pip_qt.show()'
+    label = 'pip qt'
+    tooltip = "pip qt"
 
     menus = unreal.ToolMenus.get()
     level_menu_bar = menus.find_menu('LevelEditor.LevelEditorToolBar.PlayToolBar')
@@ -26,10 +25,6 @@ def create_script_editor_button():
     level_menu_bar.add_menu_entry(section_name, entry)
     menus.refresh_all_widgets()
 
-## uncomment to add a button to the toolbar to launch your tool
-## don't forget to modify the function
-# create_script_editor_button()
+create_script_editor_button()
 
 ## TODO add code to add to menu
-
-print("My plugin is enabled")
